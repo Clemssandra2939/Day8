8d72a652b4b616efcd80ad506be536452001d1ce
Clemssandra2939 <sclems600@gmail.com>
2024-11-17 05:51:27 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+# Function with input
+
+# Review:
+# Create a function called greet()
+# Write  3 pront statement inside the function.
+# Call the greet() and run your code
+
+# def greet():
+#     print("Good morning!")
+#     print("How are you?")
+#     print("Do have a nice day!")
+
+# greet()
+
+# Wat if we can modify the function or be able to change
+
+# Function with input
+# def my_function(something):
+#     Do this with something
+#     then do this with something
+#     Finally do this with something
+
+# Function that allows for input
+# 
+def greet_with_name (name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+
+greet_with_name("Sandra")
+
+# when you run it :Hello Sandra
+# How do you do Sandra
+
+# This piece of data is then inserted in this function and you can be able to modify the function
+
+# here we have crreated a new variable called name 
+# name = Sandra
+
+# Parameter and Argument
+here name is the parameter 
+
+     
+
+
+
