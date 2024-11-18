c8a203153811a990fa50cee2d67b589a1e59e2dc
Clemssandra2939 <sclems600@gmail.com>
2024-11-18 04:23:32 +0100

    Doing her char

--- 3_positional_Keyword_Argument.py
+++ 3_positional_Keyword_Argument.py
@@ -1,1 +1,7 @@
 # Function that allows multiple input
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"wat is it like in {location}")
+
+greet_with("Sandra")
+greet with("Enugu")
