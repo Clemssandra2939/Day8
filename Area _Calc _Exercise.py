c6a8e77563118df21c95408fee92043d185d50af
Clemssandra2939 <sclems600@gmail.com>
2024-11-18 05:22:24 +0100

    Added and commented the important part

--- Area _Calc _Exercise.py
+++ Area _Calc _Exercise.py
@@ -1,2 +1,23 @@
 # ReadMe Instructions
-You are painting a wall.The instructions on the point can say that 1 can 
+# You are painting a wall.The instructions on the point can say that 1 can of paint can cover 8 square meters of wall.
+# Given a random height and width of wall,calculate how many can of paint you will need to buy.
+
+# number_of_cans =(wall height * wall width) /  coverage per can.
+
+# E.g Height = 2, Width = 4, Coverage = 5
+# number_of_cans =(2 * 4) / 5   #1.6
+
+# But bcos you can't buy 0.6 of a can of paint,the result should be rounded up to 2 cans
+
+# Important:Notice the name of the function and parameter must match those on line 13 for the code to work
+
+# Example Input:
+# test_h = 3
+# test_w = 9
+
+# Example Output:
+# You'll need 6 can of paint.
+
+# Hint:
+# To round up the number_of_can
+# Make sure you name your function/parameters the same as when it's called on the last line of code
