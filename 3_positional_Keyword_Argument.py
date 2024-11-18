3768267858d6ea153b881eba70a514a403c55524
Clemssandra2939 <sclems600@gmail.com>
2024-11-18 04:35:34 +0100

    Keyword Augument

--- 3_positional_Keyword_Argument.py
+++ 3_positional_Keyword_Argument.py
@@ -3,5 +3,19 @@ def greet_with(name,location):
     print(f"Hello {name}")
     print(f"wat is it like in {location}")
 
-greet_with("Sandra")
-greet with("Enugu")
+greet_with("Sandra", "Enugu")
+
+# when the postion of the data is switched like
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"wat is it like in {location}")
+
+greet_with("Enugu", "Sandra")
+
+# this wat it is goinh to be like
+# ##Hello Enugu
+# Wwat is it like in Sandra
+
+# placing it in the wrong parameters,this is called postional argument
+
+
