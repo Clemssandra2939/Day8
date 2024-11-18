32e1d867e06e1417a6aad09aa41142cb28280ec0
Clemssandra2939 <sclems600@gmail.com>
2024-11-18 04:42:09 +0100

    Added fo keyword argument

--- 3_positional_Keyword_Argument.py
+++ 3_positional_Keyword_Argument.py
@@ -1,16 +1,16 @@
 # Function that allows multiple input
-def greet_with(name,location):
-    print(f"Hello {name}")
-    print(f"wat is it like in {location}")
+# def greet_with(name,location):
+#     print(f"Hello {name}")
+#     print(f"wat is it like in {location}")
 
-greet_with("Sandra", "Enugu")
+# greet_with("Sandra", "Enugu")
 
 # when the postion of the data is switched like
-def greet_with(name,location):
-    print(f"Hello {name}")
-    print(f"wat is it like in {location}")
+# def greet_with(name,location):
+#     print(f"Hello {name}")
+#     print(f"wat is it like in {location}")
 
-greet_with("Enugu", "Sandra")
+# greet_with("Enugu", "Sandra")
 
 # this wat it is goinh to be like
 # ##Hello Enugu
@@ -18,4 +18,11 @@ greet_with("Enugu", "Sandra")
 
 # placing it in the wrong parameters,this is called postional argument
 
+# Keyword arguemnt
 
+# Keyword argument is a way of resolving those kind of errors of poistional argument
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"wat is it like in {location}")
+
+greet_with(location = "Enugu", name = "Sandra")
