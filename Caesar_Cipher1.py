a55fb3911e42915db2c0994299d174ced2a31d54
Clemssandra2939 <sclems600@gmail.com>
2024-11-25 05:22:54 +0100

    Added todo 1

--- Caesar_Cipher1.py
+++ Caesar_Cipher1.py
@@ -1,6 +1,7 @@
 # Part 1 - Encryption
 
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+'n','o','p','q','r','s','t','u','v','w','x','y','z''a','b','c','d','e','f','g','h','i','j','k','l','m',
 'n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 direction = input("Type 'encode' to encrypt,type 'decode' to decrypt :\n")
